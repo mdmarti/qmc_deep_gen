d8ccf8683725c60444ce1f589552324a25ce941d
mdmarti <miles.martinez@duke.edu>
2025-06-09 09:46:26 -0400

    add guassian blur gaussian lp

--- train/losses.py
+++ train/losses.py
@@ -1,5 +1,6 @@
 import torch
 from torch.nn.functional import binary_cross_entropy
+from torchvision.transforms import GaussianBlur
 
 def binary_lp(samples, data):
 
@@ -18,12 +19,12 @@ def binary_lp(samples, data):
     )
     return recon_loss
 
-def gaussian_lp(samples,data,var=1):
+def gaussian_lp(samples,data,var=1.):
 
     recon_loss = - torch.mean(
                     torch.special.logsumexp(
                         torch.sum(
-                            -torch.nn.gaussian_nll_loss(samples.swapaxes(0,1).tile((data.shape[0],1,1,1)),
+                            -torch.nn.functional.gaussian_nll_loss(samples.swapaxes(0,1).tile((data.shape[0],1,1,1)),
                                                 data.tile(1,samples.shape[0],1,1),
                                                 var=var,
                                                 reduction=None
@@ -34,4 +35,12 @@ def gaussian_lp(samples,data,var=1):
                     )
                 )
     return recon_loss
+
+def gaussian_lp_with_blur(samples,data,var=1.,kernel_size=4,sigma=0.25):
+
+    blur = GaussianBlur(kernel_size,sigma)
+
+    blur_samples,blur_data = blur(samples),blur(data)
+
+    return gaussian_lp(blur_samples,blur_data,var)
 
