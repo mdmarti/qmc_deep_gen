714b9920a99d3203ad497a7eed34bc97c1ef8767
mdmarti <miles.martinez@duke.edu>
2025-06-13 09:47:50 -0400

    update for simple celeba dset

---
+++ data/toy_dsets.py
@@ -0,0 +1,22 @@
+import torch
+from torch.data.utils import Dataset,DataLoader
+
+class CelebADsetIms(Dataset):
+
+    """
+    assumes that you're using data loaded directly in all at once,
+    rather than loading 1 image at a time
+    """
+
+    def __init__(self,ims):
+
+        self.ims = ims
+        self.len = ims.shape[0]
+    
+    def __len__(self):
+
+        return self.len
+
+    def __getitem__(self,index):
+
+        return (self.ims[index],[])
