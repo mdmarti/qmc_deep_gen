948b9d2f7c0bedbcbf3bc4df58a5c8c6b91f0d88
mdmarti <miles.martinez@duke.edu>
2025-06-23 12:18:38 -0400

    add in other training options, plot fewer points

--- qmc_mc_comparison.py
+++ qmc_mc_comparison.py
@@ -46,17 +46,20 @@ def make_qmc_model(latent_dim,device):
 def generate_test_evidence(lattices,model,loader,evidence,mc_func):
 
     qe,rqe,me =[],[],[]
+    all_labels=[]
     for lattice in lattices:
         n_points = len(lattice)
 
         qmce,rqmce,mce = [],[],[]
+        labels=[]
         gc.collect()
         torch.cuda.empty_cache()
         gc.collect()
         for batch in tqdm(loader,total=len(loader)):
             
 
-            (data,_) = batch
+            (data,ls) = batch
+            labels.append(ls)
             #data = data
             mc_pts = mc_func(n_points)
             with torch.no_grad():
@@ -70,12 +73,14 @@ def generate_test_evidence(lattices,model,loader,evidence,mc_func):
             qmce.append(qmc_evidence)
             rqmce.append(rqmc_evidence)
             mce.append(mc_evidence)
+            labels.append(ls.detach().cpu().numpy())
 
+        all_labels.append(np.hstack(labels))
         qe.append(np.hstack(qmce))
         rqe.append(np.hstack(rqmce))
         me.append(np.hstack(mce))
 
-    return qe,rqe,me
+    return qe,rqe,me,all_labels
 
 
 def mc_unif(n_points,dim):
@@ -83,17 +88,18 @@ def mc_unif(n_points,dim):
 
     return torch.rand(n_points,dim,dtype=torch.float32)
 
-def evidence_plot(qmc_est,rqmc_est,mc_est,n_lattice_points,title,save_loc='ev_plot.png'):
+def evidence_plot(qmc_est,rqmc_est,mc_est,n_lattice_points,title,save_loc='ev_plot.png',n_plot=35):
 
     ax = plt.gca()
     ii = 0
     labels = ['QMC Evidence estimate','RQMC Evidence estimate', 'MC Evidence estimate']
     lines = []
+    plot_order = np.random.choice(len(qmc_est[0]),n_plot,replace=False).squeeze()
     for n,q,r,m in zip(n_lattice_points,qmc_est,rqmc_est,mc_est):
 
-        g1 = ax.scatter([n]*len(q)+np.random.randn(len(q))*.05,q,s=3,color='tab:blue')
-        g2 = ax.scatter([n]*len(q)+np.random.randn(len(q))*.05,r,s=2,color='tab:orange')
-        g3 = ax.scatter([n]*len(q)+np.random.randn(len(q))*.05,m,s=1, color='tab:green')
+        g1 = ax.scatter([n]*n_plot+np.random.randn(len(q))*.05,q[plot_order],s=3,color='tab:blue')
+        g2 = ax.scatter([n]*n_plot+np.random.randn(len(q))*.05,r[plot_order],s=2,color='tab:orange')
+        g3 = ax.scatter([n]*n_plot+np.random.randn(len(q))*.05,m[plot_order],s=1, color='tab:green')
         if ii == 0:
             lines.append(g1)
             lines.append(g2)
@@ -111,12 +117,12 @@ def evidence_plot(qmc_est,rqmc_est,mc_est,n_lattice_points,title,save_loc='ev_pl
     plt.close()
 
 
-def run_qmc_mc_comparison_experiments(save_location,dataloc,nEpochs=300):
+def run_qmc_mc_comparison_experiments(save_location,dataloc,nEpochs=300,train_mode='rqmc'):
 
 
     ############ shared model setup ###############################
     n_workers = len(os.sched_getaffinity(0))
-
+    save_location += train_mode
     #############################
     print("loading data...")
     transform = transforms.ToTensor()
@@ -130,6 +136,11 @@ def run_qmc_mc_comparison_experiments(save_location,dataloc,nEpochs=300):
     n_lattice_points = [fib(m) for m in fib_number]
     qmc_latent_dim=2
     print("done!")
+
+    if train_mode == 'rqmc':
+        random = True 
+    else:
+        random=False
     ############## Set up qmc model, training ###################################
 
     mc_fnc = lambda n: mc_unif(n,2)
@@ -145,7 +156,14 @@ def run_qmc_mc_comparison_experiments(save_location,dataloc,nEpochs=300):
         if not os.path.isfile(save_qmc):
             print("now training qmc model")
             ## starting this run takes a little while for some reason...
-            qmc_model,qmc_opt,qmc_losses = train_qmc.train_loop(qmc_model,train_loader,train_base_sequence.to(device),qmc_loss_function,nEpochs=nEpochs)
+            if train_mode == 'mc':
+                qmc_model,qmc_opt,qmc_losses,labels = train_qmc.train_loop_mc(qmc_model,train_loader,
+                                                                          qmc_loss_function,mc_fnc,
+                                                                           nEpochs=nEpochs)
+            else:
+                qmc_model,qmc_opt,qmc_losses,labels = train_qmc.train_loop(qmc_model,train_loader,
+                                                                           train_base_sequence.to(device),qmc_loss_function,
+                                                                           nEpochs=nEpochs,random=random)
             save(qmc_model.to('cpu'),qmc_opt,qmc_losses,fn=save_qmc)
             qmc_model.to(device)
             qmc_losses = np.array(qmc_losses)
@@ -171,7 +189,7 @@ def run_qmc_mc_comparison_experiments(save_location,dataloc,nEpochs=300):
                 ev_stats = json.load(f)
             qmce,rqmce,mce =  [np.array(q) for q in ev_stats['qmc']],[np.array(r) for r in ev_stats['rqmc']],[np.array(m) for m in ev_stats['mc']]   
         ev_plot_save_fn = os.path.join(save_location,f'model_evidence_comparison_{n_train}_points_train.png')
-        evidence_plot(qmce,rqmce,mce,n_lattice_points,title=f"Model evidence estimates, trained with {f_num} lattice points",save_loc=ev_plot_save_fn)
+        evidence_plot(qmce,rqmce,mce,n_lattice_points,title=f"Model evidence estimates, trained with {n_train} lattice points",save_loc=ev_plot_save_fn)
     
 if __name__ == '__main__':
 
